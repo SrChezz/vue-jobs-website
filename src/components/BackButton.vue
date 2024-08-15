2e1c622ab73184fff404bbc755f67e7f2ab6de13
Chezzby <itzchezvil17@gmail.com>
2024-08-14 19:36:28 -0500

    first commit

---
+++ src/components/BackButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <section>
+    <div class="container m-auto py-6 px-6">
+      <RouterLink
+        to="/jobs"
+        class="text-green-600 hover:text-green-700 flex items-center"
+      >
+        <i class="pi pi-arrow-circle-left mr-3"></i> Back to Job Listings
+      </RouterLink>
+    </div>
+  </section>
+</template>
